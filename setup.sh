ef074e7633379dcaef108c3ceaf671d2264c953e
Atish Kumar Naskar <atishnaskar1999@gmail.com>
2022-12-22 06:36:15 +0530

    added a set up script for dotfiles

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+folder=$(pwd)
+
+echo $folder
+
+config_folder="$folder/config"
+
+echo $config_folder
+
+for file in $(ls $config_folder);
+do
+  source="$config_folder/$file"
+  destin="$HOME/.config/$file"
+  echo "$source --> $destin"
+  #ln -svf $source $destin
+done
+
+
+
